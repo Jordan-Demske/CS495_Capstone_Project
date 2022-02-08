cccb1f1c6f036059776fc237d3b1985558532c33
Max Braun <maxbraun175@yahoo.com>
2022-02-07 19:23:25 -0500

    update login functionality

--- login/login.php
+++ login/login.php
@@ -1,34 +1,43 @@
 <?php
-    include $_SERVER["DOCUMENT_ROOT"]."/Capstone/components/tools.php";
-    
-    $mysqli = get_mysql_connection();
-
-    if($mysqli == false){
-        print("Failed to connect to database.");
-        exit();
-    }
 
     $data = $_POST['data'];
     $username = $data[0];
     $password = $data[1];
 
-    $result = $mysqli->query("SELECT id,username,password,first_name,last_name FROM admin WHERE username = '$username'");
+    $result = logIn($username, $password);
 
-    if (mysqli_num_rows($result) > 0) {
-        // Username is found
-        // Add session variable and redirect
-        while($row = $result->fetch_assoc()){
-            if(password_verify($password, $row['password'])){
-                // Password matches
-                $_SESSION["username"] = $row["username"];
-                echo json_encode("true");
-            }else{
-                echo json_encode("false");
-            }
+    echo json_encode($result);
+
+    function logIn($username, $password){
+
+        include $_SERVER["DOCUMENT_ROOT"]."/Capstone/components/tools.php";
+
+        $mysqli = get_mysql_connection();
+
+        if($mysqli == false){
+            print("Failed to connect to database.");
+            exit();
         }
-    }else{
-        echo json_encode("false");
+
+        $result = $mysqli->query("SELECT id,username,password,first_name,last_name FROM admin WHERE username = '$username'");
+
+        if (mysqli_num_rows($result) > 0) {
+            // Username is found
+            while($row = $result->fetch_assoc()){
+                if(password_verify($password, $row['password'])){
+                    // Password matches
+                    $_SESSION["username"] = $row["username"];
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }else{
+            return false;
+        }
+
+
+        mysqli_free_result($result);
+        $mysqli->close();
     }
-    mysqli_free_result($result);
-    $mysqli->close();
 ?>
