a6542ae15ea5b9e3aa7b5d5ec0e8319a45ee2a42
jdcokerbsu <jdcoker@bsu.edu>
2022-02-07 19:36:16 -0500

    Add tests

--- components/tools.php
+++ components/tools.php
@@ -1,10 +1,14 @@
 <?php
     session_start();
-    function get_mysql_connection(){
+    function get_mysql_connection($isTest = false){
         $host = "localhost";
         $username = "root";
         $password = "";
         $dbname = "database";
+
+        if($isTest) {
+            $dbname = "test_database";
+        }
 
         $id;
         $name;
@@ -18,6 +22,30 @@
             exit();
         }else{
             return $mysqli;
+        }
+    }
+
+    class SQL_Connection{
+        private $host = "localhost";
+        private $username = "root";
+        private $password = "";
+        private $dbname = "database";
+
+        private $id;
+        private $name;
+        private $rating;
+
+        function get_mysql_connection(){
+            session_start();
+            $mysqli = new mysqli($host, $username, $password, $dbname);
+
+            if($mysqli->connect_errno ) {
+                return false;
+                $log = "Connect failed: ".$mysqli -> connect_error;
+                exit();
+            }else{
+                return $mysqli;
+            }
         }
     }
 ?>
